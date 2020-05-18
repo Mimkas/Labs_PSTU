17cc042f83e8fb44e508b66b7673477c3dac4a10
Деменев Максим Александрович АСУ-19-1БЗУ <41922095+Mimkas@users.noreply.github.com>
2020-05-19 00:34:06 +0500

    Add files via upload

---
+++ Информатика первое полугодие/6 Решение нелинейных уравнений/Лаб 6.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <stdlib.h>
+#include <cmath>
+#include <ctime>
+using namespace std;
+double Iter(double x, double eps)
+{
+	double rez; int iter = 0;
+	cout << "x0= " << x << "\n";
+	do {
+		rez = x;
+		x = x*x-sqrt(2);
+		iter++;
+	} while (fabs(x-rez) >= eps && iter < 20000);
+	return x;
+}
+double Nyut(double x, double eps)
+{
+	double f, df; int iter = 0;
+	cout << "x0= " << x << "\n";
+	do {
+		f = x * x - 2;
+		df = 2 * x;
+		x = x - f / df;
+		iter++;
+	} while (fabs(f) > eps && iter < 20000);
+	return x;
+}
+double Div(double left, double right, double eps)
+{
+	double x, fl, fr, f;
+	int iter = 0;
+	cout << "начало = " << left << " конец = " << right << "\n";
+	do {
+		x = (left + right) / 2;
+		f = x * x - 2;
+		if (f > 0) right = x;
+		else left = x;
+		iter++;
+	} while (fabs(f) > eps && iter < 20000);
+	return x;
+}
+void main()
+{
+	setlocale(LC_ALL, "Rus");
+	double left, right, eps, r;
+	cout << "Начало диапозона: ";
+	cin >> left;
+	cout << "Конец диапозона: ";
+	cin >> right;
+	cout << "Точность: ";
+	cin >> eps;
+
+	srand(time(0));
+	r = rand() % 11*0.4-2;
+
+	cout << "Результат методом итерации: " << Iter(r, eps) << "\n";
+	cout << "Результат методом ньютона: " << Nyut(r, eps) << "\n";
+	cout << "Результат методом деления: " << Div(left, right, eps) << "\n";
+}
