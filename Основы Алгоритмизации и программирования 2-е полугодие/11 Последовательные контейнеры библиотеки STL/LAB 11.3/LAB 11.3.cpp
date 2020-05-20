fe98f9c1cb6d927fba0ef9664881677440d3d8ad
Деменев Максим Александрович АСУ-19-1БЗУ <41922095+Mimkas@users.noreply.github.com>
2020-05-21 00:24:24 +0500

    Add files via upload

---
+++ Основы Алгоритмизации и программирования 2-е полугодие/11 Последовательные контейнеры библиотеки STL/LAB 11.3/LAB 11.3.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include "Vector.h"
+#include "time.h"
+using namespace std;
+void main()
+{
+    Vector<time>vec(5);
+    vec.Print();
+    cout << "\n\n\n";
+    time t = vec.Minsize();
+    vec.Add(t);
+    vec.Print();
+    cout << "\n\n\n";
+    vec = vec.Del();
+    vec.Print();
+    cout << "\n\n\n";
+    vec.AddMinMaxSize();
+    vec.Print();
+}
