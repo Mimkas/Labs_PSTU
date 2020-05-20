9fc83c7c0f8018f93e3f20e6c05edf75a152acb0
Деменев Максим Александрович АСУ-19-1БЗУ <41922095+Mimkas@users.noreply.github.com>
2020-05-20 23:27:24 +0500

    Add files via upload

---
+++ Основы Алгоритмизации и программирования 2-е полугодие/11 Последовательные контейнеры библиотеки STL/LAB11/Debug/LAB 11.4/Time.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <iostream>
+using namespace std;
+class time
+{
+private:
+	int min, sec;
+public:
+	time() { min = 0; sec = 0; }
+	time Randtime() { min = rand() % 60; sec = rand() % 60; return *this; }
+	time(int m, int s) { min = m; sec = s; }
+	time(const time& t) { min = t.min; sec = t.sec; }
+	~time() {};
+	int Getmin() { return min; }
+	int Getsec() { return sec; }
+	void Setmin(int m) { min = m; }
+	void Setsec(int s) { sec = s; }
+	time& operator=(const time&);
+	time operator-(const time&);
+	time operator+(const time&);
+	friend istream& operator>>(istream& in, time& t);
+	friend ostream& operator<<(ostream& out, const time& t);
+	bool operator!=(const time&);
+	bool operator <(const time&);
+	bool operator >(const time&);
+	bool operator==(const time&);
+
+};
